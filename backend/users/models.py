990ae2490cad528891f1cc85914748d50d9c4a82
Maksick <mezhow@ya.ru>
2026-05-15 04:31:34 +0500

    🛂 JWT-auth

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('parent', 'Parent'),
+        ('educator', 'Educator'),
+        ('medic', 'Medic'),
+        ('admin', 'Admin'),
+    )
+
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
