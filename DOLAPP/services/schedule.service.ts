890230750498cb971d4044a1db4a2645bbeb3884
Maksick <mezhow@ya.ru>
2026-05-20 03:50:10 +0500

    ✨ EducatorScreen: new

--- DOLAPP/services/schedule.service.ts
+++ DOLAPP/services/schedule.service.ts
@@ -6,4 +6,23 @@ export const scheduleService = {
 
     return response.data;
   },
+
+  async getAttendance(eventId: number) {
+    const response = await api.get(`/schedule/${eventId}/attendance/`);
+
+    return response.data;
+  },
+
+  async updateAttendance(
+    eventId: number,
+    childId: number,
+    status: "attended" | "absent",
+  ) {
+    const response = await api.post(`/schedule/${eventId}/attendance/`, {
+      child_id: childId,
+      status,
+    });
+
+    return response.data;
+  },
 };
