4bbb1175e85d061d538a475d618fceb93863428b
Maksick <mezhow@ya.ru>
2026-05-19 20:20:04 +0500

    ✨ NewsModel

---
+++ backend/news/models.py
@@ -0,0 +1,16 @@
+import uuid
+from django.db import models
+
+class News(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    body = models.TextField(blank=True)
+    image = models.ImageField(upload_to='news_images/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+    
+    def __str__(self):
+        return self.title
