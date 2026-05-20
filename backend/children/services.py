b5e9a2e9945b0cbfc8640c10b41093319b83009e
Maksick <mezhow@ya.ru>
2026-05-21 01:28:46 +0500

    ⚡️ medic: children/view

--- backend/children/services.py
+++ backend/children/services.py
@@ -2,4 +2,11 @@ from .models import Child
 
 
 def get_accessible_children(user):
-    return Child.objects.filter(parents=user).distinct()
+    if user.role == "educator":
+        return Child.objects.filter(group__educators=user)
+    if user.role == "parent":
+        return Child.objects.filter(parents=user)
+    if user.role == "medic":
+        # Медик видит всех детей (или только детей своего учреждения, если нужно)
+        return Child.objects.all()
+    return Child.objects.none()
